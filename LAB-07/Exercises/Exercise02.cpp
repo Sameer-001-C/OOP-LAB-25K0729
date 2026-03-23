ea3317cbacefc33884904261722929ac115f2f88
Shaikh Muhammad Sameer <K250729@nu.edu.pk>
2026-03-23 19:52:13 +0500

    Create Exercise02.cpp

---
+++ LAB-07/Exercises/Exercise02.cpp
@@ -0,0 +1,53 @@
+/*
+Exercise 2
+Create the following classes:
+
+Base Class: Shape
+
+Derived Classes: Circle, Rectangle
+
+Each class should have a function: draw()
+
+Override the function in each derived class.
+*/
+
+#include <iostream>
+using namespace std;
+
+
+class Shape{
+	public:
+		void draw(){
+			cout << "Drawing a shape." << endl;
+		}
+};
+
+
+class Circle : public Shape {
+	public:
+		void draw(){
+			cout << "Drawing a Circle." << endl;
+		}
+};
+
+
+class Rectangle : public Shape {
+	public:
+		void draw(){
+			cout << "Drawing a Rectangle." << endl;
+		}
+};
+
+
+int main(){
+	Shape s1;
+	Circle c1;
+	Rectangle r1;
+	
+	s1.draw();
+	c1.draw();
+	r1.draw();
+	
+	return 0;
+}
+
